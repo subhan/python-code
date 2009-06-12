43d358aa485cfbd3634bebd5761b44ca9b52d915
subhan <subhan@Gladiator.(none)>
2009-06-12 12:42:30 +0530

    webserver code is added to the repo

---
+++ webserver.py
@@ -0,0 +1,70 @@
+import threading
+import socket
+import sys,pprint
+
+CRLF = "\r\n"
+
+def start_server(host,port):
+	server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	server.bind((host,port))
+	server.listen(5)
+	print "server started on %s : %s" %(host,port)
+
+	try:
+		while True:
+			clientsocket,address = server.accept()
+			t = threading.Thread(target=handler,args=(clientsocket,))
+			t.run()
+	except KeyboardInterrupt:
+		print "server is shutting down"
+		server.close()
+
+
+class Response:
+	def __init__(self):
+		self.http_version = "HTTP/1.1"
+		self.status = "200 OK"
+		self.headers = {}
+		self.data = ""
+
+	def send(self,f):
+		f.write("%s:%s\r\n" %(self.http_version,self.status))
+		header_string = CRLF.join(["%s:%s" %(k,v) for k in self.headers.items()])
+		f.write(header_string)
+		f.write(CRLF)
+		f.write(CRLF)
+		f.write(self.data)
+		
+
+
+def handler(client):
+	f = client.makefile()
+	method,path,headers = read_headers(f)
+	pprint.pprint(headers)
+	
+	response = Response()
+
+
+def read_headers(f):
+	method,path,version = f.readline().split()
+	headers = {}
+
+	while True:
+		data = f.readline()
+		if data.strip() == "":
+			break
+		k,v = data.strip().split(":",1)
+		headers[k.strip()] = v.strip()
+
+	return method,path,headers	
+
+
+if __name__ == "__main__":
+	
+	if len(sys.argv) == 2:
+		port = int(sys.argv[1])
+	else:
+		port = 8080
+
+	start_server('localhost',port)
+	
