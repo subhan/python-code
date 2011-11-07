7a1673c9ada9a02b5267eaf27a07725d87e501aa
Mahaboob Subhan <subhan.py@gmail.com>
2011-11-07 21:29:25 +0530

    changed modified time

--- num2word.py
+++ num2word.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-"""
-    Program to convert number to words
-    @author: Prashanth
-    date: 09/08/11
-"""
-
 w1 = {
     '0':'Zero','1':'One','2':'Two','3':'Three','4':'Four','5':'Five','6':'Six','7':'Seven','8':'Eight','9':'Nine','10':'Ten','11':'Eleven','12':'Twelve','13':'Thirteen','14':'Fourteen','15':'Fifteen','16':'Sixteen','17':'Seventeen','18':'Eighteen','19':'Ninteen'
 }
